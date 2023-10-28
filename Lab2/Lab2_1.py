f8ca187697ab2ec26b250cca2bc853c37c9e2861
Fr1nd1 <97989428+Fr1nd1@users.noreply.github.com>
2023-10-28 14:58:50 +0300

    Update Lab2_1.py

--- Lab2/Lab2_1.py
+++ Lab2/Lab2_1.py
@@ -5,24 +5,15 @@ increase = 0.05  # Ежемесячный рост цен
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 month = 0
-total_amount = money_capital - spend
 
-if total_amount > 0:
-    total_amount += salary
-    month += 1
-    spend_after_increase = spend * (1 + increase)
+while money_capital >= 0:
+    # расходы на текущий месяц
+    spend_after_increase = spend * (1 + increase) ** month
+    # уменьшаем финансовую подушку безопасности
+    money_capital += salary - spend_after_increase
+    if money_capital < 0:
+        break
 
-    while total_amount - spend_after_increase > 0:
-        month += 1
-        total_amount -= spend_after_increase
-        total_amount += salary
-        spend_after_increase = spend_after_increase * (1 + increase)
+    month += 1
 
-    # Последний месяц, когда останется хоть какая-то подушка безопасности. К 1 числу следующего месяца она полностью иссякнет
-    month += 1
-    print("Количество месяцев, которое можно протянуть без долгов:", month)
-elif total_amount == 0:
-    month += 1
-    print("Количество месяцев, которое можно протянуть без долгов:", month)
-else:
-    print("Количество месяцев, которое можно протянуть без долгов:", month)
+print("Количество месяцев, которое можно протянуть без долгов:", month)
