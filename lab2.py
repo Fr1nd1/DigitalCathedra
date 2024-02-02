9296e2bd15867101cfd688fe5785b124425ee2df
Fr1nd1 <97989428+Fr1nd1@users.noreply.github.com>
2024-02-02 16:30:35 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,6 +1,15 @@
 class Book():
-    def __init__(self, id: int, name: str, pages: int):
-        self.id = id
+    def __init__(self, id_: int, name: str, pages: int):
+        '''
+        Присвоение пармаетрам книге
+        :param id_: Идентификатор книги
+        :param name: Название книги
+        :param pages: Количество страниц в книге
+
+        Примеры:
+        >>> book_A = Book(777, "Три товарища", 480)
+        '''
+        self.id_ = id_
         self.name = name
         self.pages = pages
 
@@ -8,26 +17,61 @@ class Book():
         return f"Книга {self.name}"
 
     def __repr__(self) -> str:
-        return f"Book({self.id}, {self.name!r}, {self.pages})"
+        return f"Book({self.id_}, {self.name!r}, {self.pages})"
 
 class Library():
     def __init__(self, books = None):
+        '''
+        Класс библиотеки
+        :param books: Список книг
+
+        Примеры:
+        >>> library_A = Library()
+        '''
         if books is None:
             books = []
         self.books = books
 
     def add_new_book(self, book: Book) -> None:
+        '''
+        Добавление новой книги
+        :param book: Новая книга
+        :return: Список книг с добавленной новой
+
+        Примеры:
+        >>> library_A = Library()
+        >>> library_A.add_new_book(Book(777, "Три товарища", 480))
+        '''
         self.books.append(book)
 
-    def get_next_book_id(self):
-        if len(self.books) == 0:
+    def get_next_book_id(self) -> int:
+        '''
+        Метод, возвращающий идентификатор для добавления новой книги в библиотеку.
+        :return: Идентификатор для добавления новой книги в библиотеку
+
+        Примеры:
+        >>> library_A = Library()
+        >>> library_A.add_new_book(Book(777, "Три товарища", 480))
+        '''
+        if not self.books:
             return 1
         else:
-            return self.books[-1].id + 1  # Получаем id последней книги и добавляем 1
+            return self.books[-1].id_ + 1  # Получаем id последней книги и добавляем 1
 
-    def get_index_by_book_id(self, book_id):
+    def get_index_by_book_id(self, book_id) -> int:
+        '''
+        Метод, возвращающий индекс книги в списке, который хранится в атрибуте экземпляра класса.
+        :param book_id: Индекс книги в списке
+        :return: Индекс книги в списке
+
+        Примеры:
+        >>> library_A = Library()
+        >>> library_A.add_new_book(Book(1, "Три товарища", 480))
+        >>> library_A.get_index_by_book_id(1)
+        0
+        '''
         for index, book in enumerate(self.books):
-            if book.id == book_id:
+            if book.id_ == book_id:
                 return index
         raise ValueError("Книги с запрашиваемым id не существует")
 
